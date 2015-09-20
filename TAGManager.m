63089e0fb589ba2c464c331b3b9f1a205ea10fd2
peyoo <peyoo.zh@gmail.com>
2015-09-20 14:54:29 +0800

    first commit

---
+++ TAGManager.m
@@ -0,0 +1,11 @@
+//
+//  GAI+GAI.m
+//  Pods
+//
+//  Created by cjpystudio on 20/09/2015.
+//
+//
+
+#import "TAGManager.h"
+@interface TAGManager (Dummy) @end
+@implementation TAGManager (Dummy)@end
